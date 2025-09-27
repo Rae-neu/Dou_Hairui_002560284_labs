9a2392d4791d22bec4cc31432735897cc72e0eee
Rae-neu <dou.hai@northeastern.edu>
2025-09-27 19:38:01 -0400

    Model Feature Completed

--- Lab 4/src/Model/Feature.java
+++ Lab 4/src/Model/Feature.java
@@ -9,5 +9,46 @@ package Model;
  * @author Eve Dou
  */
 public class Feature {
+    //这个类用于描述某个Product的一个具体特征。
+    private Product owner;
+    private String name;
+    private Object value;
     
+    public Feature(Product owner){
+        this.owner = owner;
+    }
+    
+    public Feature(Product owner,String name,Object value){
+        this.name = name;
+        this.value = value;
+    }
+
+    public Product getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Product owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
 }
